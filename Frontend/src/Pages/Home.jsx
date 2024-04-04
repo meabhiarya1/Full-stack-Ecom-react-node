3e6e056179c878b8f72f4e6c051defb924b32c85
Abhishek kumar <abhi.arya97161@gmail.com>
2024-04-04 22:28:16 +0530

    navbar designed responsive

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,44 +1,64 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { FaCartArrowDown } from "react-icons/fa";
+import { IoReorderThree } from "react-icons/io5";
+
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      {/* left navbar */}
-      <div className="bg-[#14628f] h-28 flex items-center justify-between">
-        <div className="flex  items-center justify-center">
-          <div className="mx-4 flex justify-center">
-            <img
-              src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/logo1-free-img-140x47.png"
-              alt="logo"
-              className="w-32 h-full"
-            />
+    <div >
+      <div className="bg-[#14628f] md:rounded-b-xl  rounded-b-2xl">
+        <div className="h-20 flex items-center justify-between w-[100vw] ">
+
+          {/* left navbar */}
+          <div className="flex items-center justify-center ">
+            <div className="md:mx-4 ml-2 flex justify-center lg:w-24 h-full w-12 md:w-20">
+              <img
+                src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/logo1-free-img-140x47.png"
+                alt="logo"
+              />
+            </div>
+
+            <div className="px-2 text-xl md:mx-2 mx-[1px] font-bold font-medium sm:font-bold text-[1rem] md:text-[20px]rem] md:text-[20px]">
+              Home
+            </div>
+
+            <div className="px-2 text-xl md:mx-2 mx-[1px] font-bold font-medium sm:font-bold text-[1rem] md:text-[20px]">Men</div>
+            <div className="px-2 text-xl md:mx-2 mx-[1px] font-bold font-medium sm:font-bold text-[1rem] md:text-[20px]">Women</div>
+            <div className="px-2 text-xl md:mx-2 mx-[1px] font-bold font-medium sm:font-bold text-[1rem] md:text-[20px]">Kids</div>
           </div>
-          <div className="px-2 text-xl mx-2 font-bold">Home</div>
-          <div className="px-2 text-xl mx-2 font-bold">Men</div>
-          <div className="px-2 text-xl mx-2 font-bold">Women</div>
-          <div className="px-2 text-xl mx-2 font-bold">Kids</div>
+
+          {/* right sidebar */}
+          <div className="flex mx-2">
+            <div className="flex items-center bg-white md:text-sm rounded-full mx-1 sm:flex hidden">
+              <input type="search" className="p-1 rounded-full border-0 text-black" placeholder="Search Here!!!" />
+              <CiSearch style={{ color: "black", marginRight: "5px", cursor: "pointer" }} />
+            </div>
+
+            <div className="bg-[#14628f] border-2 flex items-center p-2 rounded-xl hover:bg-black mx-2 hover:cursor-pointer hidden lg:flex">
+              <button className="font-medium text-sm" onClick={() => navigate("/login")}>Customer</button>
+            </div>
+
+            <div className="bg-[#14628f] border-2 flex items-center p-2 rounded-xl hover:bg-black mx-3 hover:cursor-pointer hidden lg:flex">
+              <button className="font-medium text-sm" onClick={() => navigate("/sellerlogin")}>Seller</button>
+            </div>
+
+            <div className="flex items-center text-xl md:text-3xl mr-1 sm:flex hidden">
+              <FaCartArrowDown style={{ cursor: "pointer" }} />
+            </div>
+            <div className="flex items-center text-3xl mr-1 sm:hidden flex h-40">
+              <IoReorderThree style={{ cursor: "pointer" }} />
+            </div>
+          </div>
         </div>
 
-        {/* right sidebar */}
-        <div className="flex mx-4">
-          <div className="flex items-center bg-white text-lg rounded-full mx-3">
-            <input type="search" className="p-2 rounded-full border-0" placeholder="Search Here!!!" />
-            <CiSearch style={{ color: "black", marginRight: "15px", cursor: "pointer" }} />
-          </div>
-
-          <div className="bg-[#14628f] border-2 flex items-center p-2 rounded-xl hover:bg-black mx-3">
-            <button className="font-medium text-sm">Customer Login</button>
-          </div>
-
-          <div className="bg-[#14628f] border-2 flex items-center p-2 rounded-xl hover:bg-black mx-3">
-            <button className="font-medium text-sm">Seller Login</button>
-          </div>
-
-          <div className="flex items-center text-3xl mx-2">
-            <FaCartArrowDown style={{ cursor: "pointer" }} />
-          </div>
+        <div className="flex items-center bg-white md:text-sm rounded-full sm:hidden bg-white mx-[2px]">
+          <input type="search" className="p-1 rounded-full border-0 text-black w-full" placeholder="Search Here!!!" />
+          <CiSearch style={{ color: "black", marginRight: "5px", cursor: "pointer" }} />
         </div>
       </div>
     </div>
