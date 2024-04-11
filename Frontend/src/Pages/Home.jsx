10f632b5bc9cd4dd92cb6f466fe6d1955e7ae9c7
Abhishek kumar <abhi.arya97161@gmail.com>
2024-04-11 10:18:18 +0530

    scrollbar designed

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { FaCartArrowDown } from "react-icons/fa";
 import { IoReorderThree } from "react-icons/io5";
 import { useSelector, useDispatch } from "react-redux";
 import { cartSliceActions } from "../Store/cartReducer";
+import "./scrollbar.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -53,7 +54,11 @@ const Home = () => {
               />
               <CiSearch
                 style={{
-                  color: "black", marginRight: "8px", cursor: "pointer", fontWeight: "bolder", fontSize: "1.5rem"
+                  color: "black",
+                  marginRight: "8px",
+                  cursor: "pointer",
+                  fontWeight: "bolder",
+                  fontSize: "1.5rem",
                 }}
               />
             </div>
@@ -92,7 +97,13 @@ const Home = () => {
             placeholder="Search Here!!!"
           />
           <CiSearch
-            style={{ color: "black", marginRight: "8px", cursor: "pointer", fontWeight: "bolder", fontSize: "1.5rem" }}
+            style={{
+              color: "black",
+              marginRight: "8px",
+              cursor: "pointer",
+              fontWeight: "bolder",
+              fontSize: "1.5rem",
+            }}
           />
         </div>
       </div>
@@ -100,18 +111,57 @@ const Home = () => {
       <div className="h-auto w-full flex ">
         {/* Left sidebar */}
         <div className="w-1/3 mx-2">
-          <div className="flex font-semibold justify-center p-1 my-8 sm:text-sm bg-white text-black rounded-xl ml-2 text-xs">FILTERS</div>
-          <div className="flex justify-between border-2 rounded-xl sm:p-2 p-1 items-center ml-2">
-            <div className="flex justify-center ml-4 font-semibold sm:text-sm text-xs">Brand</div>
-            <div className="sm:flex hidden"><CiSearch
-              style={{ color: "white", marginRight: "8px", cursor: "pointer", fontWeight: "bolder", fontSize: "25px" }}
-            /></div>
+          <div className="flex font-semibold justify-center p-1 my-8 sm:text-sm bg-white text-black rounded-xl ml-2 text-xs">
+            FILTERS
           </div>
-          <div className="h-[100px] overflow-y-scroll mt-4">
-            {[...new Set(productData.map(product => product.productBrand))].map((brand, index) => (
+
+          <div className="flex justify-between border-2 rounded-xl sm:p-2 p-1 items-center ml-2">
+            <div className="flex justify-center ml-4 font-semibold sm:text-sm text-xs">
+              Brand
+            </div>
+            <div className="sm:flex hidden">
+              <CiSearch
+                style={{
+                  color: "white",
+                  marginRight: "8px",
+                  cursor: "pointer",
+                  fontWeight: "bolder",
+                  fontSize: "25px",
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="h-[200px] mt-4 overflow-y-scroll scrollbar-w-2 scrollbar-track-gray-200 scrollbar-thumb-gray-500">
+            {[
+              ...new Set(productData.map((product) => product.productBrand)),
+            ].map((brand, index) => (
               <div key={index} className="flex justify-start my-3 items-center">
-                <input type="checkbox" className="mx-2 w-3 sm:w-4 h-4 cursor-pointer" />
+                <input
+                  type="checkbox"
+                  className="mx-2 w-3 sm:w-4 h-4 cursor-pointer"
+                />
                 <div className="sm:text-sm text-xs">{brand}</div>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-between border-2 rounded-xl sm:p-2 p-1 items-center ml-2 my-8">
+            <div className="flex justify-center ml-4 font-semibold sm:text-sm text-xs">
+              Price
+            </div>
+          </div>
+
+          <div className="h-[200px] mt-4 overflow-y-scroll scrollbar-w-2 scrollbar-track-gray-200 scrollbar-thumb-gray-500">
+            {[
+              ...new Set(productData.map((product) => product.productPrice)),
+            ].map((price, index) => (
+              <div key={index} className="flex justify-start my-3 items-center">
+                <input
+                  type="checkbox"
+                  className="mx-2 w-3 sm:w-4 h-4 cursor-pointer"
+                />
+                <div className="sm:text-sm text-xs">{price}</div>
               </div>
             ))}
           </div>
